f616ad0a3094a43fd1e283192a308776325983e6
otaviocosta2110 <otaviocosta23212@gmail.com>
2024-05-04 16:42:44 -0300

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -5,60 +5,57 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Central de Controle de Vôos</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <header className={styles.header}>
+
+          <ul className={styles.headerLinks}>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Visualizar Vôos</a>
+            </li>
+            <li>
+              <a href="">Atualizar Vôo</a>
+            </li>
+            <li>
+              <a href="">Deletar Vôo</a>
+            </li>
+          </ul>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Central de Controle de Vôos
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      </header>
+
+      <main>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="" className={styles.card}>
+            <h3>Visualizar Vôos </h3>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="" className={styles.card}>
+            <h3>Cadastrar Vôos</h3>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href=""
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>Atualizar Vôo</h3>
+          </a>
+
+          <a
+            className={styles.card}
+          >
+            <h3>Deletar Vôo</h3>
           </a>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
 
       <style jsx>{`
         main {
